291074912b9ac1a437fbd05c02f9a7511d325f3f
Hantao-Ye <hantaoye2000@gmail.com>
2022-01-08 18:47:58 +0800

    update BasicModel in apps/users

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -7,13 +9,19 @@ GENDER_CHOICES = (
 )
 
 
-# Create your models here.
+class BaseModel(models.Model):
+    add_time = models.DateTimeField(verbose_name="added time", default=datetime.now())
+
+    class Meta:
+        abstract = True
+
+
 class UserProfile(AbstractUser):
-    nick_name = models.CharField(verbose_name='nick name', max_length=50, default='')
-    birthday = models.DateField(verbose_name='birthday', null=True, blank=True)
-    gender = models.CharField(verbose_name='gender', choices=GENDER_CHOICES, max_length=6)
-    address = models.CharField(verbose_name='address', max_length=100, default='')
-    mobile = models.CharField(verbose_name='mobile', max_length=11, unique=True)
+    # nick_name = models.CharField(verbose_name='nick name', max_length=50, default='')
+    # birthday = models.DateField(verbose_name='birthday', null=True, blank=True)
+    # gender = models.CharField(verbose_name='gender', choices=GENDER_CHOICES, max_length=6)
+    # address = models.CharField(verbose_name='address', max_length=100, default='')
+    # mobile = models.CharField(verbose_name='mobile', max_length=11, unique=True)
     profile_image = models.ImageField(verbose_name='profile image', upload_to='profile_image/%Y/%m',
                                       default='profile_image/default.jpg')
 
@@ -22,7 +30,4 @@ class UserProfile(AbstractUser):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        if self.nick_name:
-            return self.nick_name
-
-        return self.username
+        return self.email
