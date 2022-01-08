120d803a346400fb411102b9622f190a70546b1d
Hantao-Ye <hantaoye2000@gmail.com>
2022-01-08 19:58:33 +0800

    update user operation relevant model in apps/operation

--- apps/operations/models.py
+++ apps/operations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+from apps.users.models import BaseModel
+from apps.courses.models import Course
+
+UserProfile = get_user_model()
+
+
+class UserCourse(BaseModel):
+    user = models.ForeignKey(UserProfile, verbose_name='user', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, verbose_name='course', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'user courses'
+        verbose_name_plural = verbose_name
