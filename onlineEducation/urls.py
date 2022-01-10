82854039e1893360b8671a20383198e639946977
Hantao-Ye <hantaoye2000@gmail.com>
2022-01-10 15:54:04 +0800

    update index.html and relevant resources

--- onlineEducation/urls.py
+++ onlineEducation/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
+url = 'https://127.0.0.1:8000/'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
