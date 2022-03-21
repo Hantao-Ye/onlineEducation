148238a6768b7297dc0a35e629d794e053c1f8f5
Hantao-Ye <hantaoye2000@gmail.com>
2022-03-21 13:55:07 +0800

    update frontend with axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CourseList from "./CourseList";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CourseList />
   </React.StrictMode>,
   document.getElementById('root')
 );
